34fcc2c605a2cde0987fc4c6d736cef18bc03688
hacker-pyrat <parent.j.f@gmail.com>
2016-12-16 18:57:29 +0300

    :sparkles: Form Validation System Part I;

--- client/components/ux/Form.jsx
+++ client/components/ux/Form.jsx
@@ -3,6 +3,50 @@ import validator from 'validator'
 
 import BaseComponent from 'core/BaseComponent'
 
+/* *********************************************************
+[*] Real example => client/routes/Login/components/Component.jsx
+EXAMPLE:
+  <Form ref='form'>
+    <div className='row'>
+      <div className='medium-6 columns'>
+        <MaterialInput
+          label={emailPlaceholder}
+->          validate // tell the Form that this field need to be validated
+->          isEmail // validator
+->          isRequired // validator
+->          name='email' // name is required
+          type='text'
+        />
+      </div>
+    </div>
+    <div className='row'>
+      <div className='medium-6 columns'>
+        <LaddaButton
+          name='login-btn'
+->          type='submit' // tell the Form that this button need to be disabled if the form is invalid
+          isLoading={this.props.state.login.loading}
+          onSubmit={this.onSubmit}
+        >
+          <FormattedMessage
+            id='login.SubmitBtn'
+            defaultMessage='Login'
+          />
+        </LaddaButton>
+      </div>
+    </div>
+  </Form>
+**********************************************************/
+
+// VALIDATORS CONFIG
+// Don't reinvent the wheel => https://github.com/chriso/validator.js
+// Add validators here
+// VVVVVVVVVVVVVVVVVVV
+const VALIDATORS = {
+  'isRequired': (value, args) => { return !validator.isEmpty(value || '') },
+  'isLongerThan': (value, args) => { return (value || '').length > args[0] },
+  'isEmail': (value, args) => { return validator.isEmail(value || '') }
+}
+
 class Form extends BaseComponent {
   constructor (props) {
     super(props)
@@ -21,68 +65,62 @@ class Form extends BaseComponent {
     let validatorsByName = {}
     let validationsState = {}
     let values = {}
-    const init = function (component, children, index) {
+    const injectPropsFunc = function (component, children, index) {
       let injectedProps = {}
 
       // Validated Input
+      // TODO walk the props object and if one props
+      // is satisfaying the regex /is{1}[A-Z]{1}/
+      // then inject the props
       if (component.props.validate) {
         const componentName = component.props.name
 
+        // Inject our on onChange event handler
         injectedProps['onChange'] = this.setValue
 
         // TODO what if it is not a string?
         values[componentName] = component.props['value'] || ''
 
-        // name validation
+        // the props `name` is required in order to do the mapping
         if (componentName === undefined) {
           this.error('Missing props "name" for component:', component)
           throw new Error('Fatal error in <Form />')
         }
 
+        // Init the validators configs
         validatorsByName[componentName] = []
-        // isRequired
-        if (component.props.isRequired) {
-          let effectiveValidator = this.getValidator('isRequired')
-          validatorsByName[componentName].push(effectiveValidator)
-          validators.push({
-            name: componentName,
-            validator: effectiveValidator
-          })
-        }
+        Object.keys(component.props).map((key, index) => {
+          // Match only props that start with 'is' followed by a capital letter
+          if (key.match(/is{1}[A-Z]{1}/)) {
+            let effectiveValidator = this.getValidator(key)
+            let validatorConfig = {
+              componentName: componentName,
+              name: key,
+              func: effectiveValidator,
+              extraArgs: [
+                component.props[key]
+              ]
+            }
+            validatorsByName[componentName].push(validatorConfig)
+            validators.push(validatorConfig)
+          }
+        })
 
-        // isLongerThan
-        if (component.props.isLongerThan) {
-          let effectiveValidator = (value) => { return value.length > component.props.isLongerThan }
-          validatorsByName[componentName].push(effectiveValidator)
-          validators.push({
-            name: componentName,
-            validator: effectiveValidator
-          })
-        }
-
-        // isEmail
-        if (component.props.isEmail) {
-          let effectiveValidator = this.getValidator('isEmail')
-          validatorsByName[componentName].push(effectiveValidator)
-          validators.push({
-            name: componentName,
-            validator: effectiveValidator
-          })
-        }
-
+        // Set the initial validation state of the component
         let validationState = this.validateField(validatorsByName[componentName], component.props['value'] || '')
         validationsState[componentName] = validationState
         injectedProps['validationState'] = validationState
       }
 
+      // Clone the component with the new injected props
       const clonedComponent = <component.type key={index} {...component.props} {...injectedProps}>
         {children}
       </component.type>
+
       return clonedComponent
     }.bind(this)
 
-    const children = this.traverseChildren(this.props.children, init)
-
+    const children = this.traverseChildren(this.props.children, injectPropsFunc)
     this.state = {
       validators,
       validationsState,
@@ -93,11 +131,7 @@ class Form extends BaseComponent {
   }
 
   getValidator (validatorName) {
-    if (validatorName === 'isRequired') {
-      return (value) => { return !validator.isEmpty(value) }
-    } else {
-      return validator[validatorName]
-    }
+    return VALIDATORS[validatorName]
   }
 
   traverseChildren (component, func, index = 0) {
@@ -115,24 +149,29 @@ class Form extends BaseComponent {
     }
   }
 
-  validateField (validators, value) {
+  validateField (validatorConfigs, value) {
+    let isRequired = false
+    let results = validatorConfigs.map((validatorConfig) => {
+      if (validatorConfig.name === 'isRequired') {
+        isRequired = true
+      }
+      return validatorConfig.func(value, validatorConfig.extraArgs)
+    })
+
+    // NOTE special case for isRequired
     if (value === '') {
-      if (validators.indexOf('isRequired')) {
+      if (isRequired) {
         return 'error'
       } else {
         return 'warning'
       }
-    } else {
-      let results = validators.map((validator) => {
-        return validator(value)
-      })
+    }
 
-      let result = results.reduce((a, b) => { return a && b })
-      if (result) {
-        return 'success'
-      } else {
-        return 'error'
-      }
+    let result = results.reduce((a, b) => { return a && b })
+    if (result) {
+      return 'success'
+    } else {
+      return 'error'
     }
   }
 
@@ -147,7 +186,7 @@ class Form extends BaseComponent {
 
   isFormInvalid () {
     const validations = this.state.validators.map((validatorConfig) => {
-      return validatorConfig.validator(this.state.values[validatorConfig.name])
+      return validatorConfig.func(this.state.values[validatorConfig.componentName], validatorConfig.extraArgs)
     })
     const isValid = validations.reduce((a, b) => { return a && b })
     return !isValid
@@ -155,7 +194,7 @@ class Form extends BaseComponent {
 
   render () {
     // eslint-disable-next-line
-    const init = function (component, children, index) {
+    const mapStateToProps = function (component, children, index) {
       let injectedProps = {}
 
       // Validation button
@@ -175,7 +214,8 @@ class Form extends BaseComponent {
       </component.type>
       return clonedComponent
     }.bind(this)
-    let children = this.traverseChildren(this.state.children, init)
+
+    let children = this.traverseChildren(this.state.children, mapStateToProps)
 
     return (
       <form>
